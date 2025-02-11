b221769e278eb18fed1689eeb8284c2ef0ace1eb
Miraz Noviansyah <mrazdiw@gmail.com>
2025-02-11 18:38:47 +0700

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+    return (
+        <footer className="border-t">
+            <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+                <Link href="/">
+                    <Image src="/assets/images/logo.svg" width={128} height={38} alt="Logo" />
+                </Link>
+
+                <p>2025 Evenly. All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
