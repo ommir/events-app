977ba465962246de67dc7fdb1b82852c62bfb29f
Miraz Noviansyah <mrazdiw@gmail.com>
2025-02-11 22:07:59 +0700

    debug (1)

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,53 +56,54 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === 'user.created') {
+    console.log(evt.data, 'DATAAAAAAAAAAAAAAAAAAAA');
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
-      photo: image_url,
-    };
+    // const user = {
+    //   clerkId: id,
+    //   email: email_addresses[0]?.email_address!,
+    //   username: username!,
+    //   firstName: first_name!,
+    //   lastName: last_name!,
+    //   photo: image_url,
+    // };
 
-    const newUser = await createUser(user);
+    // const newUser = await createUser(user);
 
-    if (newUser) {
-      await clerk.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
+    // if (newUser) {
+    //   await clerk.users.updateUserMetadata(id, {
+    //     publicMetadata: {
+    //       userId: newUser._id,
+    //     },
+    //   });
+    // }
 
-    return NextResponse.json({ message: 'OK', user: newUser });
+    // return NextResponse.json({ message: 'OK', user: newUser });
   }
 
-  if (eventType === 'user.updated') {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+  // if (eventType === 'user.updated') {
+  //   const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      firstName: first_name!,
-      lastName: last_name!,
-      username: username!,
-      photo: image_url,
-    };
+  //   const user = {
+  //     firstName: first_name!,
+  //     lastName: last_name!,
+  //     username: username!,
+  //     photo: image_url,
+  //   };
 
-    const updatedUser = await updateUser(id, user);
+  //   const updatedUser = await updateUser(id, user);
 
-    return NextResponse.json({ message: 'OK', user: updatedUser });
-  }
+  //   return NextResponse.json({ message: 'OK', user: updatedUser });
+  // }
 
-  if (eventType === 'user.deleted') {
-    const { id } = evt.data;
+  // if (eventType === 'user.deleted') {
+  //   const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id!);
+  //   const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({ message: 'OK', user: deletedUser });
-  }
+  //   return NextResponse.json({ message: 'OK', user: deletedUser });
+  // }
 
   return new Response('Webhook received', { status: 200 });
 }
--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher([
   '/',
   '/events/:id',
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
+  '/api/webhook(.*)',
   '/api/uploadthing',
   '/sign-in(.*)',
   '/sign-up(.*)',
